0a091fcdb68a7f0db3e318ac609b9a65d9528881
Mauricio Nagaoka <mhnagaoka@gmail.com>
2013-07-25 01:02:52 -0300

    melhoria front end

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var url = require('url')
   , session = express.session({secret: SECRET
                              , key: KEY
                              , store: store})
-  , _ = require('underscore');
+  , _ = require('underscore')
+  , https = require('https');
 
 var port = process.env.PORT || 3000;
 
@@ -25,11 +26,43 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/chat/:room/:user', function(req, res){
+app.get('/login/google', function (req, res) {
+  var nonce = Math.floor(Math.random() * 90000) + 10000;
+  req.session.nonce = nonce;
+  var urlObj = {
+    protocol: 'https',
+    host: 'accounts.google.com',
+    pathname: '/o/oauth2/auth',
+    query: {
+      client_id: '971716775291-5u2igpq9var1nkq2ssutl2kk9thp25p0.apps.googleusercontent.com',
+      response_type: 'code',
+      scope: 'openid email',
+      redirect_uri: 'http://localhost:3000/oauth2callback',
+      state: nonce
+    }
+  };
+  res.redirect(url.format(urlObj));
+});
+app.get('/oauth2callback', function (req, res) {
+  var urlObj = url.parse(req.url, true);
+  if (req.session.nonce == urlObj.query.state) {
+  	console.log('nonce ok.');
+  }
+  var accessTokenUrlObj = {
+    code: urlObj.query.code,
+    client_id: '971716775291-5u2igpq9var1nkq2ssutl2kk9thp25p0.apps.googleusercontent.com',
+    client_secret: 'alRwAgjQ6yKR-0F3e8sECt8L',
+    redirect_uri: 'http://localhost:3000/code',
+    grant_type: 'authorization_code'
+  };
+  //TODO pending
+  //https.request()
+  res.end();
+});
+app.get('/chat/:room/:user', function (req, res) {
   res.render('index');
 });
-
-server.listen(port, function(){
+server.listen(port, function () {
   console.log("Express e Socket.IO no ar. Porta " + port);
 });
 
---
+++ test.js
@@ -0,0 +1,2 @@
+console.log(encodeURIComponent('http://google.com'));
+console.log(Math.floor(Math.random() * 90000) + 10000)
